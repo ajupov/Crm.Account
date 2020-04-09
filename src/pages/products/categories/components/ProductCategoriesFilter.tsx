3e51cb02ba49eebd3ac467152b524b0f9d307db3
Usman Ayupov <u.ayupov@dodopizza.com>
2020-04-09 22:19:17 +0300

    Fix filtering

--- src/pages/products/categories/components/ProductCategoriesFilter.tsx
+++ src/pages/products/categories/components/ProductCategoriesFilter.tsx
@@ -1,71 +1,25 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable array-bracket-newline */
+import { Button, Checkbox, Form, Header, Input } from 'semantic-ui-react'
+import React, { FC } from 'react'
 
-import { Button, Checkbox, CheckboxProps, Form, Header, Input, InputOnChangeData } from 'semantic-ui-react'
-import React, { FC, useCallback, useContext } from 'react'
-
-import ProductCategoriesContext from '../contexts/ProductCategoriesContext'
 import useProductCategoriesFilters from '../hooks/useProductCategoriesFilters'
 
 const ProductCategoriesFilter: FC = () => {
-    const { request, setRequest } = useContext(ProductCategoriesContext)
-
     const {
         name,
-        setName,
+        onChangeName,
         isDeleted,
-        setIsDeleted,
+        onChangeIsDeleted,
         minCreateDate,
-        setMinCreateDate,
+        onChangeMinCreateDate,
         maxCreateDate,
-        setMaxCreateDate,
+        onChangeMaxCreateDate,
         minModifyDate,
-        setMinModifyDate,
+        onChangeMinModifyDate,
         maxModifyDate,
-        setMaxModifyDate
+        onChangeMaxModifyDate,
+        onSubmit,
+        onReset
     } = useProductCategoriesFilters()
-
-    const onSubmit = useCallback(() => {
-        setRequest({ ...request, name, isDeleted, minCreateDate, maxCreateDate, minModifyDate, maxModifyDate })
-    }, [isDeleted, maxCreateDate, maxModifyDate, minCreateDate, minModifyDate, name, request, setRequest])
-
-    const onClickClear = useCallback(() => {
-        setName(void 0)
-        setIsDeleted(false)
-        setMinCreateDate(void 0)
-        setMaxCreateDate(void 0)
-        setMinModifyDate(void 0)
-        setMaxModifyDate(void 0)
-
-        setRequest({ ...request, name, isDeleted, minCreateDate, maxCreateDate, minModifyDate, maxModifyDate })
-    }, [setIsDeleted, setMaxCreateDate, setMaxModifyDate, setMinCreateDate, setMinModifyDate, setName])
-
-    const onChangeName = useCallback(
-        (_, { value }: InputOnChangeData) => {
-            setName(value)
-        },
-        [setName]
-    )
-
-    const onChangeMinCreateDate = useCallback((_, data: InputOnChangeData) => () => setMinCreateDate(data.value), [
-        setMinCreateDate
-    ])
-
-    const onChangeMaxCreateDate = useCallback((_, data: InputOnChangeData) => () => setMaxCreateDate(data.value), [
-        setMaxCreateDate
-    ])
-
-    const onChangeMinModifyDate = useCallback((_, data: InputOnChangeData) => () => setMinModifyDate(data.value), [
-        setMinModifyDate
-    ])
-
-    const onChangeMaxModifyDate = useCallback((_, data: InputOnChangeData) => () => setMaxModifyDate(data.value), [
-        setMaxModifyDate
-    ])
-
-    const onChangeIsDeleted = useCallback((_, data: CheckboxProps) => setIsDeleted(data.value === 'true'), [
-        setIsDeleted
-    ])
 
     return (
         <>
@@ -92,28 +46,29 @@ const ProductCategoriesFilter: FC = () => {
                 <Form.Field>
                     <label>Статус:</label>
                     <Checkbox
-                        size="mini"
                         radio
+                        size="mini"
                         label="Действующие"
                         value="false"
-                        name="checkboxRadioGroup"
                         checked={isDeleted === false}
                         onChange={onChangeIsDeleted}
                     />
                     <Checkbox
-                        size="mini"
                         radio
+                        size="mini"
                         label="Удаленные"
                         value="true"
-                        name="checkboxRadioGroup"
                         checked={isDeleted}
                         onChange={onChangeIsDeleted}
                     />
                 </Form.Field>
+
                 <Form.Field>
                     <Button.Group size="mini" floated="right">
-                        <Button type="reset" basic onClick={onClickClear} content="Сброc" />
-                        <Button type="submit" content="Применить" />
+                        <Button type="reset" basic onClick={onReset}>
+                            Сброс
+                        </Button>
+                        <Button type="submit">Применить</Button>
                     </Button.Group>
                 </Form.Field>
             </Form>
--- src/pages/products/categories/hooks/useProductCategoriesFilters.ts
+++ src/pages/products/categories/hooks/useProductCategoriesFilters.ts
@@ -1,41 +1,107 @@
-import { useState } from 'react'
+/* eslint-disable array-bracket-newline */
+
+import { CheckboxProps, InputOnChangeData } from 'semantic-ui-react'
+import { useCallback, useContext, useState } from 'react'
+
+import ProductCategoriesContext from '../contexts/ProductCategoriesContext'
 
 interface UseProductCategoriesFiltersReturn {
-    name?: string
-    setName: (value?: string) => void
-    isDeleted?: boolean
-    setIsDeleted: (value?: boolean) => void
-    minCreateDate?: string
-    setMinCreateDate: (value?: string) => void
-    maxCreateDate?: string
-    setMaxCreateDate: (value?: string) => void
-    minModifyDate?: string
-    setMinModifyDate: (value?: string) => void
-    setMaxModifyDate: (value?: string) => void
-    maxModifyDate?: string
+    name: string
+    onChangeName: (_: any, { value }: InputOnChangeData) => void
+    isDeleted: boolean
+    onChangeIsDeleted: (_: any, { value }: CheckboxProps) => void
+    minCreateDate: string
+    onChangeMinCreateDate: (_: any, { value }: InputOnChangeData) => void
+    maxCreateDate: string
+    onChangeMaxCreateDate: (_: any, { value }: InputOnChangeData) => void
+    minModifyDate: string
+    onChangeMinModifyDate: (_: any, { value }: InputOnChangeData) => void
+    maxModifyDate: string
+    onChangeMaxModifyDate: (_: any, { value }: InputOnChangeData) => void
+    onSubmit: () => void
+    onReset: () => void
 }
 
 const useProductCategoriesFilters = (): UseProductCategoriesFiltersReturn => {
-    const [name, setName] = useState<string | undefined>()
-    const [isDeleted, setIsDeleted] = useState<boolean | undefined>(false)
-    const [minCreateDate, setMinCreateDate] = useState<string | undefined>()
-    const [maxCreateDate, setMaxCreateDate] = useState<string | undefined>()
-    const [minModifyDate, setMinModifyDate] = useState<string | undefined>()
-    const [maxModifyDate, setMaxModifyDate] = useState<string | undefined>()
+    const { request, setRequest } = useContext(ProductCategoriesContext)
+
+    const [name, setName] = useState<string>('')
+    const [isDeleted, setIsDeleted] = useState<boolean>(false)
+    const [minCreateDate, setMinCreateDate] = useState<string>('')
+    const [maxCreateDate, setMaxCreateDate] = useState<string>('')
+    const [minModifyDate, setMinModifyDate] = useState<string>('')
+    const [maxModifyDate, setMaxModifyDate] = useState<string>('')
+
+    const onChangeName = useCallback((_, { value }: InputOnChangeData) => setName(value), [setName])
+
+    const onChangeIsDeleted = useCallback((_, data: CheckboxProps) => setIsDeleted(data.value === 'true'), [
+        setIsDeleted
+    ])
+
+    const onChangeMinCreateDate = useCallback((_, data: InputOnChangeData) => setMinCreateDate(data.value), [
+        setMinCreateDate
+    ])
+
+    const onChangeMaxCreateDate = useCallback((_, data: InputOnChangeData) => setMaxCreateDate(data.value), [
+        setMaxCreateDate
+    ])
+
+    const onChangeMinModifyDate = useCallback((_, data: InputOnChangeData) => setMinModifyDate(data.value), [
+        setMinModifyDate
+    ])
+
+    const onChangeMaxModifyDate = useCallback((_, data: InputOnChangeData) => setMaxModifyDate(data.value), [
+        setMaxModifyDate
+    ])
+
+    const onSubmit = useCallback(() => {
+        setRequest({
+            ...request,
+            name,
+            isDeleted,
+            minCreateDate,
+            maxCreateDate,
+            minModifyDate,
+            maxModifyDate,
+            offset: 0
+        })
+    }, [isDeleted, maxCreateDate, maxModifyDate, minCreateDate, minModifyDate, name, request, setRequest])
+
+    const onReset = useCallback(() => {
+        setName('')
+        setIsDeleted(false)
+        setMinCreateDate('')
+        setMaxCreateDate('')
+        setMinModifyDate('')
+        setMaxModifyDate('')
+
+        setRequest({
+            ...request,
+            name: '',
+            isDeleted: false,
+            minCreateDate: '',
+            maxCreateDate: '',
+            minModifyDate: '',
+            maxModifyDate: '',
+            offset: 0
+        })
+    }, [request, setRequest])
 
     return {
         name,
-        setName,
+        onChangeName,
         isDeleted,
-        setIsDeleted,
+        onChangeIsDeleted,
         minCreateDate,
-        setMinCreateDate,
+        onChangeMinCreateDate,
         maxCreateDate,
-        setMaxCreateDate,
+        onChangeMaxCreateDate,
         minModifyDate,
-        setMinModifyDate,
+        onChangeMinModifyDate,
         maxModifyDate,
-        setMaxModifyDate
+        onChangeMaxModifyDate,
+        onSubmit,
+        onReset
     }
 }
 
