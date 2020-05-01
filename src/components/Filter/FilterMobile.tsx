9fd717a652fe9733f9bd4bc417547db85c2a1345
Usman Ayupov <u.ayupov@dodopizza.com>
2020-05-02 00:51:28 +0300

    Fix lint

--- src/components/Filter/FilterMobile.tsx
+++ src/components/Filter/FilterMobile.tsx
@@ -17,28 +17,26 @@ const FilterMobile: FC<FilterMobile & FilterProps> = ({
     onApply,
     isResetEnabled,
     onReset
-}) => {
-    return (
-        <>
-            <Button basic icon size="mini" floated="right" onClick={onShow}>
-                <Icon name="filter" />
-                Фильтры
-            </Button>
-            <Modal size="mini" open={isShow} onClose={onHide}>
-                <Card fluid>
-                    <Card.Content>
-                        <Filter
-                            fields={fields}
-                            isApplyEnabled={isApplyEnabled}
-                            onApply={onApply}
-                            isResetEnabled={isResetEnabled}
-                            onReset={onReset}
-                        />
-                    </Card.Content>
-                </Card>
-            </Modal>
-        </>
-    )
-}
+}) => (
+    <>
+        <Button basic icon size="mini" floated="right" onClick={onShow}>
+            <Icon name="filter" />
+            Фильтры
+        </Button>
+        <Modal size="mini" open={isShow} onClose={onHide}>
+            <Card fluid>
+                <Card.Content>
+                    <Filter
+                        fields={fields}
+                        isApplyEnabled={isApplyEnabled}
+                        onApply={onApply}
+                        isResetEnabled={isResetEnabled}
+                        onReset={onReset}
+                    />
+                </Card.Content>
+            </Card>
+        </Modal>
+    </>
+)
 
 export default FilterMobile
