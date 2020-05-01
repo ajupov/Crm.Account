fdae16610dbb54a45046341dd95c1bb89b1d841c
Usman Ayupov <u.ayupov@dodopizza.com>
2020-05-01 23:44:47 +0300

    Move mobile filters

--- src/pages/Products/components/ProductCategories/components/ProductsCategoriesFilterMobile/ProductsCategoriesFilterMobile.tsx
+++ src/pages/Products/components/ProductCategories/components/ProductsCategoriesFilterMobile/ProductsCategoriesFilterMobile.tsx
@@ -1,26 +1,22 @@
-import { Button, Card, Icon, Modal } from 'semantic-ui-react'
 import React, { FC, useContext } from 'react'
 
-import ProductCategoriesFilter from '../ProductCategoriesFilter/ProductCategoriesFilter'
+import FilterMobile from '../../../../../../components/Filter/FilterMobile'
 import ProductCategoriesFiltersContext from '../../contexts/ProductCategoriesFiltersContext/ProductCategoriesFiltersContext'
 
 const ProductsCategoriesFilterMobile: FC = () => {
     const state = useContext(ProductCategoriesFiltersContext)
 
     return (
-        <>
-            <Button basic icon size="mini" floated="right" onClick={state.onShowMobile}>
-                <Icon name="filter" />
-                Фильтры
-            </Button>
-            <Modal size="mini" open={state.isShowMobile} onClose={state.onHideMobile}>
-                <Card fluid>
-                    <Card.Content>
-                        <ProductCategoriesFilter />
-                    </Card.Content>
-                </Card>
-            </Modal>
-        </>
+        <FilterMobile
+            isShow={state.isShowMobile}
+            onShow={state.onShowMobile}
+            onHide={state.onHideMobile}
+            fields={state.fields}
+            isApplyEnabled={state.isApplyEnabled}
+            onApply={state.onApply}
+            isResetEnabled={state.isResetEnabled}
+            onReset={state.onReset}
+        />
     )
 }
 
